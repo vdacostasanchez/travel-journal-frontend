8a750d7dd59018584e18110a091e94e5cbc44401
vdacostasanchez <v.dacostasanchez@gmail.com>
2023-12-28 18:20:51 -0600

    initial commit

---
+++ src/Content.jsx
@@ -0,0 +1,7 @@
+export function Content() {
+  return (
+    <main>
+      <h1>Welcome to your Travel Journal!</h1>
+    </main>
+  );
+}
---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <h1>Travel Journal</h1>
+    </header>
+  );
+}
